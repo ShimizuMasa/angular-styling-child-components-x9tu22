9b7cbadeaee0343aef456ad0ceac1bd1a5da5977
ShimizuMasa <116647120+ShimizuMasa@users.noreply.github.com>
2022-10-25 09:22:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-styling-child-components-x9tu22
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-styling-child-components-x9tu22)
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [AppComponent, ParentComponent, ChildComponent],
+  imports: [BrowserModule],
+})
+export class AppModule {}
